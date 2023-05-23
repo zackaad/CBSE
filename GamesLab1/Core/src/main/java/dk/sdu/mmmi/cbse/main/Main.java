187de3ba2cc1a73387c68e9f17b3f820825cb654
zackaad <73196460+zackaad@users.noreply.github.com>
2023-05-23 22:41:04 +0200

    1st

---
+++ GamesLab1/Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -0,0 +1,17 @@
+package dk.sdu.mmmi.cbse.main;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Asteroids");
+		config.setWindowSizeLimits(1000,500,1000,400);
+
+		new Lwjgl3Application(new Game(), config);
+	}
+	
+}
